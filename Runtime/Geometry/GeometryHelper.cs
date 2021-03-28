5f618750b71bc4fc15f5ceedcdf5dba199bbb920
herman <pickgliss@gmail.com>
2021-03-28 17:06:24 +0800

    CreateCorridor

--- Runtime/Geometry/GeometryHelper.cs
+++ Runtime/Geometry/GeometryHelper.cs
@@ -15,38 +15,5 @@ namespace Pickgliss.Geometry
             result.Add(new Line2D(rect.RightBottom,rect.LeftBottom));
             return result;
         }
-
-        public static Line2D GetRectConnected(Rect r1,Rect r2)
-        {
-            Line2D line = new Line2D();
-            if(r1.Left == r2.Right)
-            {
-                line.p0.x = line.p1.x = r1.Left;
-                line.p0.y = math.max(r1.Bottom,r2.Bottom);
-                line.p1.y = math.min(r1.Top,r2.Top);
-            }
-            else if(r1.Right == r2.Left)
-            {
-                line.p0.x = line.p1.x = r1.Right;
-                line.p0.y = math.max(r1.Bottom,r2.Bottom);
-                line.p1.y = math.min(r1.Top,r2.Top);
-
-            }else if(r1.Bottom == r2.Top)
-            {
-                line.p0.y = line.p1.y = r1.Bottom;
-                line.p0.x = math.max(r1.Left,r2.Left);
-                line.p1.x = math.min(r1.Right,r2.Right);
-
-            }else if(r1.Top == r2.Bottom)
-            {
-                line.p0.y = line.p1.y = r1.Top;
-                line.p0.x = math.max(r1.Left,r2.Left);
-                line.p1.x = math.min(r1.Right,r2.Right);
-            }else
-            {
-                Debug.LogWarning("GetRectConnected Rect No connections");
-            }
-            return line;
-        }
     }
 }
