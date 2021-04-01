aa9b1c193aadcbd656703c11b870b644187ef28c
herman <pickgliss@gmail.com>
2021-04-01 17:02:43 +0800

    Direction

---
+++ Runtime/Attribute/EnumFlagsAttribute.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEditor;
+namespace Pickgliss.Attribute
+{
+    public class EnumFlagsAttribute : PropertyAttribute
+    {
+        public EnumFlagsAttribute() { }
+    }
+    #if UNITY_EDITOR
+    [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+    public class EnumFlagsAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+        }
+    }
+    #endif
+}
---
+++ Runtime/Geometry/Direction.cs
@@ -0,0 +1,16 @@
+using System;
+namespace Pickgliss.Geometry
+{
+    [Flags]
+    public enum Direction : int
+    {
+        All = -1,
+        None = 0,
+        Left = 1 << 0,
+        Right = 1 << 1,
+        Front = 1 << 2,
+        Back = 1 << 3,
+        Top = 1 << 4,
+        Bottom = 1 << 5
+    }
+}
