cf6fc156440e15ff9025586d3fb4f75ac87c3a38
herman <pickgliss@gmail.com>
2021-04-16 07:05:18 +0800

    EnumExtension

--- Runtime/Extension/EnumExtension.cs
+++ Runtime/Extension/EnumExtension.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
+
 public static class EnumExtension
 {
-    static IEnumerable<Enum> GetFlags(Enum input)
+    public static IEnumerable<Enum> GetFlags(Enum input)
     {
         foreach (Enum value in Enum.GetValues(input.GetType()))
             if (input.HasFlag(value))
