92e5e459a28215885df00ab7bc9c27327d28bcb7
herman <pickgliss@gmail.com>
2021-04-16 10:32:50 +0800

    Walls

--- Runtime/Geometry/Direction.cs
+++ Runtime/Geometry/Direction.cs
@@ -7,20 +7,18 @@ namespace Pickgliss.Geometry
     [Flags]
     public enum Direction : int
     {
-        None = 0,
         Left = 1 << 0,
         Right = 1 << 1,
         Front = 1 << 2,
         Back = 1 << 3,
         Bottom = 1 << 4,
         Top = 1 << 5,
+        Walls = Left | Right | Front | Back,
         
     }
 
-    [Flags]
     public enum Direction2D : int
     {
-        None = 0,
         Left = Direction.Left,
         Right = Direction.Right,
         Front = Direction.Front,
