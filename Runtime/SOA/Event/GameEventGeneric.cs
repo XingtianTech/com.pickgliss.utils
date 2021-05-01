dab262093ed30727daf6dbb68f36c9e851351f5e
pickgliss <pickgliss@gmail.com>
2021-05-01 16:13:36 +0800

    GameEvent

---
+++ Runtime/SOA/Event/GameEventGeneric.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Pickgliss.SOA
+{
+    public class GameEventGeneric<T> : ScriptableObject
+    {
+        private readonly HashSet<Action<T>> _actions = new HashSet<Action<T>>();
+       // rest almost same as GameEvent
+  }
+}
