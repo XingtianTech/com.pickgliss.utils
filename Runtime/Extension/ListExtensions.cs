c728f18a1b1433a99e54b19dd4e0613d51c1a373
pickgliss <pickgliss@gmail.com>
2021-05-16 11:14:06 +0800

    Shuffle Random

--- Runtime/Extension/ListExtensions.cs
+++ Runtime/Extension/ListExtensions.cs
@@ -4,12 +4,12 @@ using Unity.Mathematics;
 
 public static class ListExtensions
 {
-    public static void Shuffle<T>(this IList<T> list)  
+    public static void Shuffle<T>(this IList<T> list,Random random)  
     {  
         int n = list.Count;  
         while (n > 1) {  
             n--;  
-            int k = RandomHelper.Rng.NextInt(n + 1);  
+            int k = random.NextInt(n + 1);  
             T value = list[k];  
             list[k] = list[n];  
             list[n] = value;  
