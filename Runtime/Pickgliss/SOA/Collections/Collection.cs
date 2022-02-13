a3c8caf1912db7647f25bf8eac723bff62355ee0
pickgliss <pickgliss@gmail.com>
2022-02-13 15:24:46 +0800

    new function to call Base

--- Runtime/Pickgliss/SOA/Collections/Collection.cs
+++ Runtime/Pickgliss/SOA/Collections/Collection.cs
@@ -13,7 +13,7 @@ namespace Pickgliss.SOA.Collections
             return items[index];
         }
 
-        public virtual T GetRandomOne(T exclude)
+        public T GetRandomOne(T exclude)
         {
             while (true)
             {
