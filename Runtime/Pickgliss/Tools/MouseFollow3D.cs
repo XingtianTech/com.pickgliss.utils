4beb1f75375b6aa36970b69b2077333d99f27c14
pickgliss <pickgliss@gmail.com>
2022-02-24 10:14:36 +0800

    Drag OK

--- Runtime/Pickgliss/Tools/MouseFollow3D.cs
+++ Runtime/Pickgliss/Tools/MouseFollow3D.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Pickgliss.Extension;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,7 @@ namespace Pickgliss.Tools
     public class MouseFollow3D : MonoBehaviour
     {
         protected Camera mainCamera;
-        public InputAction holdAction;
+        public InputActionReference holdAction;
         protected Vector3 velocity = Vector3.zero;
         public float mouseDragSpeed = .1f;
         private void Awake()
@@ -15,27 +16,45 @@ namespace Pickgliss.Tools
             mainCamera = Camera.main;
         }
 
-        private void OnDisable()
-        {
-            holdAction.performed -= ActionPerformed;
-            holdAction.Disable();
-        }
-
         private void OnEnable()
         {
-            holdAction.Enable();
-            holdAction.performed += ActionPerformed;
+            holdAction.action.Enable();
+            holdAction.action.started += startFollow;
+            holdAction.action.performed += Holding;
+            holdAction.action.canceled += endFollow;
+        }
+        
+        private void OnDisable()
+        {
+            holdAction.action.started -= startFollow;
+            holdAction.action.performed -= Holding;
+            holdAction.action.canceled -= endFollow;
+            holdAction.action.Disable();
         }
 
-        protected virtual void ActionPerformed(InputAction.CallbackContext callbackContext)
+
+        protected virtual void endFollow(InputAction.CallbackContext callbackContext)
         {
+            Debug.Log($"MouseFollow3D endFollow {callbackContext}");
+        }
+
+        protected virtual void startFollow(InputAction.CallbackContext callbackContext)
+        {
+            Debug.Log($"MouseFollow3D startFollow {callbackContext}");
             StartCoroutine(DragUpdate());
         }
 
-        private IEnumerator DragUpdate()
+
+        protected virtual void Holding(InputAction.CallbackContext callbackContext)
+        {
+            Debug.Log($"MouseFollow3D Holding {callbackContext}");
+            // StartCoroutine(DragUpdate());
+        }
+
+        protected virtual IEnumerator DragUpdate()
         {
             var initialDistance = Vector3.Distance(transform.position, mainCamera.transform.position);
-            while (holdAction.ReadValue<float>() != 0)
+            while (holdAction.action.ReadValue<float>() != 0)
             {
                 var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
                 var position = transform.position;
