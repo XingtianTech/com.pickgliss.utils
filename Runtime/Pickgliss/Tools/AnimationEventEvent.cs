1fc676d17de8e010308fc3777a32a9f026aefdb4
pickgliss <pickgliss@gmail.com>
2022-03-07 13:39:09 +0800

    AnimationAttack

---
+++ Runtime/Pickgliss/Tools/AnimationEventEvent.cs
@@ -0,0 +1,10 @@
+
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Pickgliss.Tools
+{
+    [Serializable]
+    public class AnimationEventEvent : UnityEvent<AnimationEvent> { }
+}
