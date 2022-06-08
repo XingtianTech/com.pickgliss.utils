94d0c27c2af68110826c3b05559d421bf992545f
pickgliss <pickgliss@gmail.com>
2022-06-08 18:26:56 +0800

    SceneManagement

--- Runtime/Pickgliss/Extension/FolderReference.cs
+++ Runtime/Pickgliss/Extension/FolderReference.cs
@@ -3,11 +3,11 @@ using UnityEditor;
 
 namespace Pickgliss.Extension
 {
-    [System.Serializable]
-    public class FolderReference
-    {
-        public string guid;
-        public string Path => AssetDatabase.GUIDToAssetPath(guid);
-        public string AbsolutePath => PathHelper.ResolveAssetPath(Path);
-    }
+    // [System.Serializable]
+    // public class FolderReference
+    // {
+    //     public string guid;
+    //     public string Path => AssetDatabase.GUIDToAssetPath(guid);
+    //     public string AbsolutePath => PathHelper.ResolveAssetPath(Path);
+    // }
 }
--- Runtime/Pickgliss/SOA/Collections/ResourcesObjects.cs
+++ Runtime/Pickgliss/SOA/Collections/ResourcesObjects.cs
@@ -6,34 +6,34 @@ using UnityEngine;
 
 namespace Pickgliss.SOA.Collections
 {
-    [CreateAssetMenu(fileName = "ResourcesObjects",menuName = "Collection/Resources Objects")]
-    public class ResourcesObjects :  ResourceFolder<GameObject>
-    {
-        
-    }
+    // [CreateAssetMenu(fileName = "ResourcesObjects",menuName = "Collection/Resources Objects")]
+    // public class ResourcesObjects :  ResourceFolder<GameObject>
+    // {
+    //     
+    // }
     
-    public class ResourceFolder<T> : Collection<T> where T : Object
-    {
-        public FolderReference folder;
-
-        private void Awake()
-        {
-            Update();
-        }
-
-        public void OnValidate()
-        {
-            Update();
-        }
-
-        public void Update()
-        {
-            var resourcePath = PathHelper.AssetPathToResourcePath(folder.Path);
-            items ??= new List<T>();
-            items.Clear();
-            var res = Resources.LoadAll<T>(resourcePath);
-            items.AddRange(res);
-            EditorUtility.SetDirty(this);
-        }
-    }
+    // public class ResourceFolder<T> : Collection<T> where T : Object
+    // {
+    //     public FolderReference folder;
+    //
+    //     private void Awake()
+    //     {
+    //         Update();
+    //     }
+    //
+    //     public void OnValidate()
+    //     {
+    //         Update();
+    //     }
+    //
+    //     public void Update()
+    //     {
+    //         var resourcePath = PathHelper.AssetPathToResourcePath(folder.Path);
+    //         items ??= new List<T>();
+    //         items.Clear();
+    //         var res = Resources.LoadAll<T>(resourcePath);
+    //         items.AddRange(res);
+    //         EditorUtility.SetDirty(this);
+    //     }
+    // }
 }
