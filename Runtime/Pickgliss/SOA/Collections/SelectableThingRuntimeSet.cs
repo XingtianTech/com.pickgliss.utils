aa903b9fff4c6cd5e545af2e1ea8fd92b5a944f1
pickgliss <pickgliss@gmail.com>
2022-08-17 20:27:57 +0800

    HeroList

--- Runtime/Pickgliss/SOA/Collections/SelectableThingRuntimeSet.cs
+++ Runtime/Pickgliss/SOA/Collections/SelectableThingRuntimeSet.cs
@@ -1,20 +1,20 @@
-﻿using Pickgliss.SOA.Events;
-using UnityEngine;
+﻿// using Pickgliss.SOA.Events;
+// using UnityEngine;
 
-namespace Pickgliss.SOA.Collections
-{
-    [CreateAssetMenu(fileName = "SelectableGroup",menuName = "Collection/SelectableGroup")]
-    public class SelectableThingRuntimeSet : ThingRuntimeSet
-    {
-        public Thing selected;
-        public GameEvent selectChangeEvent;
-        public bool Select(Thing item)
-        {
-            // if (!items.Contains(item)) return false;
-            if (selected == item) return false;
-            selected = item;
-            selectChangeEvent.Raise();
-            return true;
-        }
-    }
-}
+// namespace Pickgliss.SOA.Collections
+// {
+//     [CreateAssetMenu(fileName = "SelectableGroup",menuName = "Collection/SelectableGroup")]
+//     public class SelectableThingRuntimeSet : ThingRuntimeSet
+//     {
+//         public Thing selected;
+//         public GameEvent selectChangeEvent;
+//         public bool Select(Thing item)
+//         {
+//             // if (!items.Contains(item)) return false;
+//             if (selected == item) return false;
+//             selected = item;
+//             selectChangeEvent.Raise();
+//             return true;
+//         }
+//     }
+// }
