3487173587a2e36f55426e6c7deba1ba5a654e57
pickgliss <pickgliss@gmail.com>
2023-03-10 15:04:39 +0800

    Update MethodName.cs

--- Runtime/Pickgliss/Helper/MethodName.cs
+++ Runtime/Pickgliss/Helper/MethodName.cs
@@ -4,5 +4,6 @@ namespace Pickgliss.Helper
     public static class MethodName
     {
         public static string Disable = "Disable";
+        public static string Deactivate = "Deactivate";
     }
 }
