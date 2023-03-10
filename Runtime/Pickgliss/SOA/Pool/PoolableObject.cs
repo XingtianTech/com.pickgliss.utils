bab5c3d939d07caa5f190dcd1d8fc9dd8b38ddf6
pickgliss <pickgliss@gmail.com>
2023-03-10 21:17:00 +0800

    Update PoolableObject.cs

--- Runtime/Pickgliss/SOA/Pool/PoolableObject.cs
+++ Runtime/Pickgliss/SOA/Pool/PoolableObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Pickgliss.SOA.Pool
 {
-    [AddComponentMenu( "SOA/PoolableObject" )]
+    [AddComponentMenu( "SOA/Poolable" )]
     public class PoolableObject : MonoBehaviour
     {
         public ObjectPool Parent;
