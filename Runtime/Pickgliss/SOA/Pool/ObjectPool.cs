1e085e9682494f8bb154a1ecfa26c2035c82c6b8
pickgliss <pickgliss@gmail.com>
2023-03-11 11:20:02 +0800

    Update ObjectPool.cs

--- Runtime/Pickgliss/SOA/Pool/ObjectPool.cs
+++ Runtime/Pickgliss/SOA/Pool/ObjectPool.cs
@@ -1,52 +1,78 @@
-
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace Pickgliss.SOA.Pool
 {
-    [CreateAssetMenu(menuName = "Utils/Collection/ObjectPool")]
-    public  class ObjectPool : ScriptableObject
+    public class ObjectPool
     {
-        public PoolableObject prefab;
-        // public int size;
-        private Stack<PoolableObject> pool = new Stack<PoolableObject>();
-        
-        // void Awake()
-        // {
-        //     pool.Clear();
-        //     for (int i = 0; i < size; i++)
-        //     {
-        //         CreateObject(); // PoolableObject handles re-adding the object to the AvailableObjects
-        //     }
-        // }
+        private GameObject Parent;
+        private PoolableObject Prefab;
+        private int Size;
+        private List<PoolableObject> AvailableObjectsPool;
+        private static Dictionary<PoolableObject, ObjectPool> ObjectPools = new Dictionary<PoolableObject, ObjectPool>();
+
+        private ObjectPool(PoolableObject Prefab, int Size)
+        {
+            this.Prefab = Prefab;
+            this.Size = Size;
+            AvailableObjectsPool = new List<PoolableObject>(Size);
+        }
+
+        public static ObjectPool CreateInstance(PoolableObject Prefab, int Size)
+        {
+            ObjectPool pool = null;
+
+            if (ObjectPools.ContainsKey(Prefab))
+            {
+                pool = ObjectPools[Prefab];
+            }
+            else
+            {
+                pool = new ObjectPool(Prefab, Size);
+
+                pool.Parent = new GameObject(Prefab + " Pool");
+                pool.CreateObjects();
+
+                ObjectPools.Add(Prefab, pool);
+            }
+            
+
+            return pool;
+        }
+
+        private void CreateObjects()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                CreateObject();
+            }
+        }
 
         private void CreateObject()
         {
-            PoolableObject poolableObject = GameObject.Instantiate(prefab);
+            PoolableObject poolableObject = GameObject.Instantiate(Prefab, Vector3.zero, Quaternion.identity, Parent.transform);
             poolableObject.Parent = this;
             poolableObject.gameObject.SetActive(false); // PoolableObject handles re-adding the object to the AvailableObjects
         }
 
         public PoolableObject GetObject()
         {
-            if (pool.Count == 0) // auto expand pool size if out of objects
+            if (AvailableObjectsPool.Count == 0) // auto expand pool size if out of objects
             {
                 CreateObject();
             }
 
-            // PoolableObject instance = pool[0];
+            PoolableObject instance = AvailableObjectsPool[0];
 
-            // pool.RemoveAt(0);
-            var item = pool.Pop();
+            AvailableObjectsPool.RemoveAt(0);
 
-            item.gameObject.SetActive(true);
+            instance.gameObject.SetActive(true);
 
-            return item;
+            return instance;
         }
 
         public void ReturnObjectToPool(PoolableObject Object)
         {
-            pool.Push(Object);
+            AvailableObjectsPool.Add(Object);
         }
     }
-} 
+}
